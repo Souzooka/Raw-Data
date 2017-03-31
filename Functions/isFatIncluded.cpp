15cc81a00981f5508ac4ddbcebb5c34d31e6ff50
Dakota <bazookaharwell@gmail.com>
2017-03-31 13:15:12 -1000

    minor: refactor isFatIncluded.cpp

--- Functions/isFatIncluded.cpp
+++ Functions/isFatIncluded.cpp
@@ -15,12 +15,5 @@ bool isFatIncluded(std::ifstream &InputFile)
     char buffer[4];
     InputFile >> buffer;
 
-    if (!strcmp(buffer, "FAT"))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return !strcmp(buffer, "FAT");
 }
