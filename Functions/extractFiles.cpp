7da7c02b0b5953c8564b10b350c987d7fce1e79d
Dakota <bazookaharwell@gmail.com>
2017-04-01 14:00:34 -1000

    not exactly creating all the subdirectories, but it's something

--- Functions/extractFiles.cpp
+++ Functions/extractFiles.cpp
@@ -1,6 +1,7 @@
 #include <inttypes.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <stdlib.h>
 #include <stdio.h>
 #define BOOST_SYSTEM_NO_DEPRECATED
@@ -12,7 +13,18 @@
 // we need a helper function that'll locate a .FAT with the same name in this directory
 int extractFiles(std::ifstream &inputFile, std::string outputFolder, int numOfFiles, std::string * fileNames, int * fileLengths, int * fileLocations, int fileOffset)
 {
-    std::cout << outputFolder;
     boost::filesystem::create_directory(outputFolder);
+    boost::filesystem::current_path(outputFolder);
+    std::string filename;
+    std::ofstream files;
+
+    for (int i = 0; i < numOfFiles; ++i)
+    {
+        std::stringstream a;
+        a << fileNames[i];
+        filename = a.str();
+        files.open(filename.c_str());
+        files.close();
+    }
     return 0;
 }
