81cfb9256f81e895f860d0c7649966a2d57ef4d5
Dakota <bazookaharwell@gmail.com>
2017-04-03 16:07:55 -1000

    some organizational stuff

--- helpers.h
+++ helpers.h
@@ -1,10 +1,13 @@
 #ifndef HELPERS_H_INCLUDED
 #define HELPERS_H_INCLUDED
 
-bool isFatIncluded(std::ifstream &InputFile);
-uint32_t getNumberOfFiles(std::ifstream &InputFile);
+// helper functions
 std::string getFileNameFromPath(std::string inputFilePath);
 std::string getPath(std::string inputFilePath);
+
+// for extracting archives
+bool isFatIncluded(std::ifstream &InputFile);
+uint32_t getNumberOfFiles(std::ifstream &InputFile);
 uint32_t getFileNameDataStartPtr(std::ifstream &inputFile);
 uint32_t getFileDataStartPtr(std::ifstream &inputFile);
 int* getFileNamePtrs(std::ifstream &inputFile);
@@ -13,6 +16,8 @@ int* getFileDataPtrs(std::ifstream &inputFile);
 std::string* getFileNames(std::ifstream &inputFile);
 int extractFiles(std::ifstream &inputFile, std::string outputFolder, int numOfFiles, std::string * fileNames, int * fileLengths, int * fileLocations, int fileOffset);
 bool isRDFile(std::ifstream &inputFile);
+
+// for rebuilding archives
 int rebuildEmptyFile(std::string fileName);
 int getFilesInDirectory (std::string filePath);
 
