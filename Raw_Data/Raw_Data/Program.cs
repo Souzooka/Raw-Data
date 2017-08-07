4eafde1a3440de653322e0b75131b5a0c4a17189
Souzooka <bazookaharwell@gmail.com>
2017-08-07 01:38:05 -1000

    Set OpenFileDialog InitialDirectory/Filter for *.DAT, tentative handling of no file selected

--- Raw_Data/Raw_Data/Program.cs
+++ Raw_Data/Raw_Data/Program.cs
@@ -16,11 +16,19 @@ namespace Raw_Data
             string path = "";
 
             OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Directory.GetCurrentDirectory();
+            ofd.Filter = "IREM Data Archive (*.DAT)|*.DAT";
             DialogResult dr = ofd.ShowDialog();
 
             if (dr == DialogResult.OK)
             {
                 path = ofd.FileName;
+            }
+            else
+            {
+                // Tentative: this will be replaced and better handled once Form1 is set up
+                // this aborts the program if user doesn't select a file
+                return;
             }
 
             // Extractor.Extract(path);
