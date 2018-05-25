cefaf2b4a8e11c4b9e306a627542261b83c52331
Souzooka <bazookaharwell@gmail.com>
2018-05-25 01:22:35 -1000

    Major rewrite to follow OOP paradigms and make code more understandable

--- Raw_Data/Raw_Data/Extractor.cs
+++ Raw_Data/Raw_Data/Extractor.cs
@@ -9,86 +9,49 @@ namespace Raw_Data
 	public static class Extractor
 	{
         // Prepended to directories created with Extract, e.g. "ROOT.DAT" -> "@ROOT.DAT"
-        private static char folderSymbol = '@';
+        public const char FolderSymbol = '@';
 
         public static void Extract(string path)
 		{
-			BinaryReader headerReader;
-			BinaryReader bodyReader = new BinaryReader(File.Open(path, FileMode.Open));
-			int fileDataOffset;
-			int fileCount;
-			int step;
-			List<string> fileNames;
-			List<int> fileLengths;
-			List<int> fileLocations;
+            FileInfo pathInfo = new FileInfo(path);
 
-			// Determine if the .DAT header is contained within the file, or an external file
-			if (IsFAT(bodyReader))
-			{
-				long temp = bodyReader.BaseStream.Position;
-
-				// If internal, read the file data offset from the header and offset all file reads by that later.
-				bodyReader.BaseStream.Position = 0xFC;
-				fileDataOffset = bodyReader.ReadInt32();
-				headerReader = bodyReader;
-
-				bodyReader.BaseStream.Position = temp;
-			}
-			else
-			{
-                // If not a header, Open .FAT in same folder
-                if (File.Exists(FindFAT(path)))
-                {
-                    headerReader = new BinaryReader(File.Open(FindFAT(path), FileMode.Open));
-                    fileDataOffset = 0;
-                }
-                else
-                {
-                    // If we cannot find header, abort
-                    // Console.WriteLine($"Warning: Could not find header for {path}!");
-                    bodyReader.Close();
-                    return;
-                }
-			}
-
-			// Gather all necessary file information
-			fileCount = GetFileCount(headerReader);
-
-            // If .DAT file is empty, abort
-            if (fileCount == 0)
+            // If invalid file or header missing, abort
+            if (!Header.IsHeader(pathInfo) || !Header.FindHeader(pathInfo).Exists)
             {
-                HandleEmptyDAT(path);
-                headerReader.Close();
-                bodyReader.Close();
                 return;
             }
 
-            // Raw Danger has padding in its headers, so the amount to seek is different between the two variants
-            step = ((IsRDFAT(headerReader)) ? 0x8 : 0xC);
+            // Gather all necessary file information
+            Header fat = new Header(pathInfo);
 
-            fileNames = GetFullPaths(GetFileNames(headerReader, fileCount), path);
-			fileLengths = GetFileLengths(headerReader, fileCount, step);
-			fileLocations = GetFileLocations(headerReader, fileCount, step);
+            // If .DAT file is empty, abort
+            if (fat.IsEmpty)
+            {
+                HandleEmptyDAT(path);
+                return;
+            }
 
-			// Iterate over each file in the archive
-			for (int i = 0; i < fileCount; ++i)
+            BinaryReader bodyReader = new BinaryReader(File.Open(path, FileMode.Open));
+
+            // Iterate over each file in the archive
+            for (int i = 0; i < fat.FileCount; ++i)
 			{
 
 				byte[] buffer = new Byte[1024];
 				int bytesRead;
 
 				// Variable which holds how much data is left to read for one file
-				int targetBytes = fileLengths[i];
+				int targetBytes = fat.FileLengths[i];
 
 				// Create directory for the file
-				if (!Directory.Exists(Path.GetDirectoryName(fileNames[i])))
+				if (!Directory.Exists(Path.GetDirectoryName(fat.FullFileNames[i])))
 				{
-					Directory.CreateDirectory(Path.GetDirectoryName(fileNames[i]));
+					Directory.CreateDirectory(Path.GetDirectoryName(fat.FullFileNames[i]));
 				}
 
 				// Create a new file in the directory with the data in the .DAT file
-				BinaryWriter writer = new BinaryWriter(File.Open(fileNames[i], FileMode.Create));
-				bodyReader.BaseStream.Position = fileLocations[i] + fileDataOffset;
+				BinaryWriter writer = new BinaryWriter(File.Open(fat.FullFileNames[i], FileMode.Create));
+				bodyReader.BaseStream.Position = fat.FileLocations[i];
 
 				// Extraction loop for 1 file
 				while (true)
@@ -112,194 +75,28 @@ namespace Raw_Data
 				writer.Close();
 			}
 
-            // Close the header stream
-            headerReader.Close();
-
             // Close .DAT stream and clean up
             bodyReader.Close();
 		}
 
-		public static string FindFAT(string path)
-		{
-			// Returns a path with the file extension changed to .FAT
-			// TODO: Check if file exists and if not handle error, operation cannot resume at this point
-			return Path.ChangeExtension(path, ".FAT");
-		}
-
-		public static int GetFileCount(BinaryReader reader)
-		{
-			// Retain current reader's position
-			long temp = reader.BaseStream.Position;
-
-			// Set reader's position to the correct position
-			reader.BaseStream.Position = 0x4;
-
-			int result = reader.ReadInt32();
-
-			// Restore reader's position
-			reader.BaseStream.Position = temp;
-
-			return result;
-		}
-
-		public static List<int> GetFileLengths(BinaryReader reader, int count, int step)
-		{
-			// Retain current reader's position
-			long temp = reader.BaseStream.Position;
-
-			// Change reader's position to the pointer data pointer, and then increment by 4 bytes to align with file lengths
-			reader.BaseStream.Position = 0xF4;
-			reader.BaseStream.Position = reader.ReadInt32() + 0x4;
-
-			List<int> result = new List<int>();
-
-			// Read each length and push into result
-			for (int i = 0; i < count; ++i)
-			{
-				result.Add(reader.ReadInt32());
-				reader.BaseStream.Position += step;
-			}
-
-			// Restore reader's position
-			reader.BaseStream.Position = temp;
-			return result;
-		}
-
-		public static List<int> GetFileLocations(BinaryReader reader, int count, int step)
-		{
-			// Retain current reader's position
-			long temp = reader.BaseStream.Position;
-
-			reader.BaseStream.Position = 0xF4;
-			reader.BaseStream.Position = reader.ReadInt32();
-
-			List<int> result = new List<int>();
-
-			for (int i = 0; i < count; ++i)
-			{
-				result.Add(reader.ReadInt32());
-				reader.BaseStream.Position += step;
-			}
-
-			// Restore reader's position
-			reader.BaseStream.Position = temp;
-
-			return result;
-		}
-
-		public static List<string> GetFileNames(BinaryReader reader, int count)
-		{
-			// Retain current reader's position
-			long temp = reader.BaseStream.Position;
-
-			// Move reader to the file names pointer and then move to that location
-			reader.BaseStream.Position = 0xF8;
-			reader.BaseStream.Position = reader.ReadInt32();
-
-			List<string> result = new List<string>();
-
-			// Iterate over each file
-			for (int i = 0; i < count; ++i)
-			{
-				List<char> charArr = new List<char>();
-
-				// while making sure we haven't hit the end of the file
-				while (reader.BaseStream.Position != reader.BaseStream.Length)
-				{
-					// read each null terminated file name
-					char letter = reader.ReadChar();
-					if (letter == (char)0) { break; }
-					charArr.Add(letter);
-				}
-
-				result.Add(String.Concat(charArr));
-			}
-
-			// Restore reader's position
-			reader.BaseStream.Position = temp;
-
-			return result;
-		}
-
-		// Changes paths found in a header file to a full relative path from the program, for easier file creation
-		public static List<string> GetFullPaths(List<string> fileNames, string path)
-		{
-			
-
-			// Prepend a character to our archive so that we can create a folder with its name
-			path = Path.Combine(Path.GetDirectoryName(path), folderSymbol + Path.GetFileName(path));
-
-			return fileNames.Select(v =>
-			{
-					// Filter out '\' or '/' and replace with the correct DirectorySeparatorChar for the system
-					v = v.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
-
-					// Combine our directory path with the file path contained inside the archive header file
-					return Path.Combine(path, v);
-
-			}).ToList();
-		}
-
         public static void HandleEmptyDAT(string path)
         {
-            if (!Directory.Exists(Path.Combine(Path.GetDirectoryName(path), folderSymbol + Path.GetFileName(path))))
+            if (!Directory.Exists(Path.Combine(Path.GetDirectoryName(path), FolderSymbol + Path.GetFileName(path))))
             {
-                Directory.CreateDirectory(Path.Combine(Path.GetDirectoryName(path), folderSymbol + Path.GetFileName(path)));
+                Directory.CreateDirectory(Path.Combine(Path.GetDirectoryName(path), FolderSymbol + Path.GetFileName(path)));
             }
         }
-
-		// Returns true if the current file is a header file
-		public static bool IsFAT(BinaryReader reader)
-		{
-			// Retain current reader's position
-			long temp = reader.BaseStream.Position;
-
-			// Set reader's position to the start of file
-			reader.BaseStream.Position = 0x0;
-
-			// Concatenate the first four bytes into a string
-			string result = String.Concat(reader.ReadChars(4));
-
-			// Restore reader's position
-			reader.BaseStream.Position = temp;
-
-			// All headers start with "FAT ", so if this file does it is (probably) a header
-			return result == "FAT ";
-		}
-
-		// Returns true if the current file is a Raw Danger header
-		public static bool IsRDFAT(BinaryReader reader)
-		{
-			// Retain current reader's position
-			long temp = reader.BaseStream.Position;
-
-			// Set reader's position to the position which points to the start of filenames
-			reader.BaseStream.Position = 0xF8;
-
-			int fnStart = reader.ReadInt32();
-
-			// Set reader's position to the (expected) position for the first filename's pointer
-			reader.BaseStream.Position = 0x108;
-
-			int fn1 = reader.ReadInt32();
-
-			// Restore reader's position
-			reader.BaseStream.Position = temp;
-
-			// In a RD FAT, fnStart should equal fn1
-			return fnStart == fn1;
-		}
 
         public static void RecursiveExtract(string path)
         {
             Extractor.Extract(path);
 
             // Move to the folder we extracted to
-            string extractedFolder = Path.Combine(Path.GetDirectoryName(path), folderSymbol + Path.GetFileName(path));
+            string extractedFolder = Path.Combine(Path.GetDirectoryName(path), FolderSymbol + Path.GetFileName(path));
 
             if (Directory.Exists(extractedFolder))
             {
-                Directory.SetCurrentDirectory(Path.Combine(Path.GetDirectoryName(path), folderSymbol + Path.GetFileName(path)));
+                Directory.SetCurrentDirectory(Path.Combine(Path.GetDirectoryName(path), FolderSymbol + Path.GetFileName(path)));
             }
             else
             {
---
+++ Raw_Data/Raw_Data/Header.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Raw_Data
+{
+    public class Header
+    {
+
+        private string fileName;
+        private FileInfo fat;
+        public int FileCount;
+        public int FileDataOffset;
+        private string[] fullFileNames;
+        public string[] FullFileNames
+        {
+            get
+            {
+                if (fullFileNames == null)
+                {
+                    // Prepend a character to our archive so that we can create a folder with its name
+                    string path = Path.Combine(fat.DirectoryName, Extractor.FolderSymbol + fileName);
+
+                    fullFileNames = FileNames.Select(v =>
+                    {
+                        // Filter out '\' or '/' and replace with the correct DirectorySeparatorChar for the system
+                        v = v.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
+
+                        // Combine our directory path with the file path contained inside the archive header file
+                        return Path.Combine(path, v);
+
+                    }).ToArray();
+                }
+
+                return fullFileNames;
+            }
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                return FileCount == 0;
+            }
+        }
+        public string[] FileNames;
+        public int[] FileLengths;
+        public int[] FileLocations;
+
+        public Header(FileInfo pFat)
+        {
+            fileName = pFat.Name;
+            if (!IsHeader(pFat))
+            {
+                pFat = FindHeader(pFat);
+                if (!pFat.Exists)
+                {
+                    throw new ArgumentException("Invalid file type or header file not found.");
+                }
+            }
+            fat = pFat;
+
+            int step;
+            if (this.IsEmpty) { step = 0; }
+            else { step = (IsRDFat(pFat) ? 0xC : 0x10); }
+
+            BinaryReader pFatBR = new BinaryReader(pFat.Open(FileMode.Open));
+
+            // FileCount
+            pFatBR.BaseStream.Position = 0x4;
+            FileCount = pFatBR.ReadInt32();
+
+            // FileDataOffset
+            pFatBR.BaseStream.Position = 0xFC;
+            FileDataOffset = pFatBR.ReadInt32();
+
+            // FileNames, FileLengths, FileLocations
+            FileNames = new string[FileCount];
+            FileLengths = new int[FileCount];
+            FileLocations = new int[FileCount];
+
+            pFatBR.BaseStream.Position = 0xF4;
+            int ptr = pFatBR.ReadInt32();
+
+            for (int i = 0; i < FileCount; ++i)
+            {
+                pFatBR.BaseStream.Position = ptr + (step * i);
+
+                // Locations
+                FileLocations[i] = pFatBR.ReadInt32() + FileDataOffset;
+
+                // Lengths
+                FileLengths[i] = pFatBR.ReadInt32();
+
+                // Names
+                pFatBR.BaseStream.Position = pFatBR.ReadInt32();
+
+                // Read null-terminated string at this location
+                StringBuilder sb = new StringBuilder();
+                byte c = pFatBR.ReadByte();
+
+                while (c != 0)
+                {
+                    sb.Append((char)c);
+                    c = pFatBR.ReadByte();
+                }
+                FileNames[i] = sb.ToString();
+
+            }
+
+            pFatBR.Close();
+        }
+
+        public static bool IsRDFat(FileInfo pFat)
+        {
+            BinaryReader pFatBR = new BinaryReader(pFat.Open(FileMode.Open));
+
+            pFatBR.BaseStream.Position = 0x4;
+            if (pFatBR.ReadInt32() == 0)
+            {
+                // empty DAT
+                return false;
+            }
+
+            // Set reader's position to the position which points to the start of filenames
+            pFatBR.BaseStream.Position = 0xF8;
+            int fnStart = pFatBR.ReadInt32();
+
+            // Set reader's position to the (expected) position for the first filename's pointer
+            pFatBR.BaseStream.Position = 0x108;
+            int fn1 = pFatBR.ReadInt32();
+
+            // Cleanup
+            pFatBR.Close();
+
+            // In a RD FAT, fnStart should equal fn1
+            return fnStart == fn1;
+        }
+
+        public static bool IsHeader(FileInfo pFat)
+        {
+            FileStream pFatFS = pFat.OpenRead();
+            StreamReader pFatSR = new StreamReader(pFatFS, System.Text.Encoding.UTF8);
+            string identifier;
+
+            char[] buffer = new char[4];
+            pFatSR.Read(buffer, 0, 4);
+            identifier = String.Concat(buffer);
+            pFatSR.Close();
+            pFatFS.Close();
+
+            return identifier == "FAT ";
+        }
+
+        public static FileInfo FindHeader(FileInfo pFat)
+        {
+            return new FileInfo(Path.ChangeExtension(pFat.FullName, ".FAT"));
+        }
+    }
+}
